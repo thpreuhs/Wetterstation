6ab129ae3594b7ec051f3aefb8a3603af03912e2
alpreuhs <alexander.preuhs@gmx.net>
2016-09-05 21:42:35 +0200

    Add additional column for wind dir in degree

--- HelpFunctions.py
+++ HelpFunctions.py
@@ -29,4 +29,42 @@ def convert_to_timeseries(file):
     tmp_time = file['Date'] + file['Time']
     file['Date_Time'] = pd.to_datetime(tmp_time, format="%d.%m.%y%H:%M")
     file = file.drop(["Date", 'Time'], axis=1)
+    file['Wind Dir Degree'] = file['Wind Dir'].apply(__wtoi)
     return file.set_index('Date_Time', drop=True)
+
+
+def __wtoi(wd):
+    if wd == 'N':
+        return 0*22.5
+    if wd == 'NNE':
+        return 1*22.5
+    if wd == 'NE':
+        return 2*22.5
+    if wd == 'ENE':
+        return 3*22.5
+    if wd == 'E':
+        return 4*22.5
+    if wd == 'ESE':
+        return 5*22.5
+    if wd == 'SE':
+        return 6*22.5
+    if wd == 'SSE':
+        return 7*22.5
+    if wd == 'S':
+        return 8*22.5
+    if wd == 'SSW':
+        return 9*22.5
+    if wd == 'SW':
+        return 10*22.5
+    if wd == 'WSW':
+        return 11*22.5
+    if wd == 'W':
+        return 12*22.5
+    if wd == 'WNW':
+        return 13*22.5
+    if wd == 'NW':
+        return 14*22.5
+    if wd == 'NNW':
+        return 15*22.5
+    if wd == '---':
+        return 16*22.5
